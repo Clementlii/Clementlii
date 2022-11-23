91f181af5840c0759abae35b135e1333440decb1
Clément Liagre <79091748+Clementlii@users.noreply.github.com>
2022-11-23 12:57:13 +0100

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,29 @@
+/*script curseur*/
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top:" + (e.pageY - 20) + "px; left:" + (e.pageX - 20) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
+
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleModal)
+);
+
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+  consol: console.log(OK);
+}
